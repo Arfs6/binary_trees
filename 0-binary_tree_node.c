714c90e334bbf064f7288ca4bb5e952f822718c4
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-02-28 13:58:30 +0100

    added gitgnore for a.out

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_node - create a new node
+ * @parent: parent of the new node
+ * @value: value to add to the new node
+ * Return: pointer to the new node
+ * NULL: insufficient memory
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL); /* insufficient memory */
+
+	new->n = value;
+	new->left = new->right = NULL;
+	new->parent = parent;
+
+	if (new->parent == NULL) /* root */
+		return new
+
+			if (new->n < parent->n)
+				parent->left = new;
+			else
+				parent->right = new;
+
+	return new;
+}
