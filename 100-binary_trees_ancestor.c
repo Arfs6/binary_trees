9b40c392c682e8cdde23a70e4887693a4f1b8dc3
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2023-03-03 12:26:31 +0100

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -11,7 +11,8 @@
  * Return: lowest common ancestor or
  * NULL: no common ancestor
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	binary_tree_t *f_parent, *s_parent;
 
